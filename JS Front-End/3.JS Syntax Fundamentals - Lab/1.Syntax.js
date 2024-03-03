c47c8fe3d2dfa9a37963a5fc2b047c5c070157d1
RTodorinov <155956729+RTodorinov@users.noreply.github.com>
2024-03-03 13:25:56 +0200

    Create 1.Syntax.js

---
+++ JS Front-End/3.JS Syntax Fundamentals - Lab/1.Syntax.js
@@ -0,0 +1,22 @@
+// Declare variables
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+var c = 30; // used before ES2015 old style (legacy) for same thing above and not recommended to use
+console.log(c + a);
+
+const pi = 3.14; // Constant variable
+
+// Conditional Statements
+if (a < b) {
+    console.log('a is lower then 10');
+} else if (a < 20) {
+    console.log('a is lower than 20');
+} else {
+    console.log('a is higher than or equal to 20')
+}
+
+expression
+statements
+
