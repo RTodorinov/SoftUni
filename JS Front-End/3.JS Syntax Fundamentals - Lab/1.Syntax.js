67524fd65af2783716846cb251b08e96c61c9fb9
RTodorinov <155956729+RTodorinov@users.noreply.github.com>
2024-03-05 22:46:50 +0200

    Update 1.Syntax.js

--- JS Front-End/3.JS Syntax Fundamentals - Lab/1.Syntax.js
+++ JS Front-End/3.JS Syntax Fundamentals - Lab/1.Syntax.js
@@ -14,9 +14,30 @@ if (a < b) {
 } else if (a < 20) {
     console.log('a is lower than 20');
 } else {
-    console.log('a is higher than or equal to 20')
+    console.log('a is higher than or equal to 20');
 }
 
-expression
-statements
+// expression // трябва да има ;
+// statements // не трябва да има ;
 
+// Function declaration
+function add(first, second) {
+    console.log(first + second);
+}
+
+// Function invocation(calling)
+add(1, pi);
+
+// Console print with string concatenation
+console.log('The number pi is: ' + pi + '!')
+
+// String interpolation
+console.log(`The number pi is: ${pi}!!`);
+
+// fix the number output
+console.log(pi.toFixed(2));
+
+let num = 5.19923;
+console.log(num.toFixed(2));
+
+console.log(10 + '2');
